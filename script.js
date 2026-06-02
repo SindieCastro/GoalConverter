1e8944435bd79093521b45a9f8097aff598ac0cb
Sindie Castro <78306719+SindieCastro@users.noreply.github.com>
2026-06-02 09:36:22 -0400

    Update script.js

--- script.js
+++ script.js
@@ -95,6 +95,7 @@ function saveGoal(goal) {
   localStorage.setItem("goals", JSON.stringify(goals));
 }
 
+
 function displayGoals() {
   var list = document.getElementById("savedGoals");
   list.innerHTML = "";
@@ -103,10 +104,23 @@ function displayGoals() {
 
   for (var i = 0; i < goals.length; i++) {
     var li = document.createElement("li");
-    li.innerText = goals[i];
+
+    li.innerText = goals[i] + " ";
+
+    var btn = document.createElement("button");
+    btn.innerText = "❌";
+
+    btn.onclick = (function(index) {
+      return function() {
+        deleteGoal(index);
+      };
+    })(i);
+
+    li.appendChild(btn);
     list.appendChild(li);
   }
 }
+
 
 window.onload = displayGoals;
 
