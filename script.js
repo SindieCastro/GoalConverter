b9c3a26da96d4f7263952a5a61cf15c459768890
Sindie Castro <78306719+SindieCastro@users.noreply.github.com>
2026-06-02 08:39:12 -0400

    Update script.js

--- script.js
+++ script.js
@@ -22,17 +22,7 @@ function convertGoal() {
 function generateGoal(input, timeframe) {
   var text = input.toLowerCase();
 
-if (text.includes("increase")) {if (text         " over " + timeframe + " days";
-}
-  var cleaned = input.replace("increase", "").trim();
-  return "✅ Achieve a 15–25% improvement in " + cleaned +
-         " within " + timeframe + " days with weekly tracking";
-}
-if (text.includes("decrease")) {if (text         " over " + timeframe + " days";
-}
-  var cleaned = input.replace("decrease", "").trim();
-  return "✅ Achieve a 10–20% reduction in " + cleaned + " over " + timeframe + " days";
-}
+
  if (text.includes("exercise") || text.includes("fitness")) {
     return "✅ Perform 4 workouts per week for " + timeframe + " days";
   }
